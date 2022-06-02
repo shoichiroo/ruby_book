f4dda8f0dce2b302d73762db854d632d8e9d1bd8
shoichiroo <shoichiroo0601@gmail.com>
2022-06-02 08:53:25 +0000

    Update

--- 2.rb
+++ 2.rb
@@ -54,8 +54,153 @@ greet(nil)
 
 %!helloworld!
 
+def some_method
+  <<~TEXT
+  \ これはヒアドキュメントです。
+  \ 複数行にわたる長い文字列を作成するのに便利です。
+  TEXT
+end
+
+name = "Alice"
 a = <<TEXT
-これはヒアドキュメントです。
-複数行にわたる長い文字列を作成するのに便利です。
+ようこそ、#{name}さん!
+以下のメッセージをご覧ください。
 TEXT
-puts a
+
+a = "Ruby"
+a.prepend(<<TEXT)
+Java
+Python
+TEXT
+
+c = [<<TEXT1, <<TEXT2]
+Alice
+Bob
+TEXT1
+Mats
+Jnchito
+TEXT2
+
+sprintf("%0.3f", 1.2)
+
+[10, 20, 30].join
+"Hi!" * 10
+
+1 && false && 3
+
+nil || false
+
+s = ""
+if s.empty?
+  "空文字です"
+end
+
+n = 123
+if !n.zero?
+  "ゼロではありません"
+end
+
+user = nil
+if !user
+  "nilです"
+end
+
+if user.nil?
+  "nilです"
+end
+
+country = "italy"
+case country
+when "japan"
+  "こんにちは"
+when "us"
+  "Hello"
+when "italy"
+  "Ciao"
+else
+  "???"
+end
+
+n = 11
+n > 10 ? "10より大きい" : "10以下"
+
+def greet(country = "japan")
+  if country == "japan"
+    "こんにちは"
+  else
+    "hello"
+  end
+end
+
+greet
+
+def default_args(a, b, c = 0, d = 0)
+  "#{a}, #{b}, #{c}, #{d}"
+end
+
+default_args(1, 2, 3, 4)
+
+def foo(time = Time.now, message = bar)
+  "time: #{time}, message: #{message}"
+end
+
+def bar
+  "BAR"
+end
+
+def point(x, y = x)
+  "#{x}, #{y}"
+end
+
+point(3, 10)
+
+"".empty?
+
+"watch".include?("tc")
+
+2.odd?
+
+2.even?
+
+"abc".nil?
+
+def multiple_of_three?(n)
+  n % 3 == 0
+end
+
+multiple_of_three?(3)
+
+a = "ruby"
+a.upcase
+
+a
+
+b = "ruby"
+b.upcase!
+
+b
+
+def reverse_upcase(s)
+  s.reverse.upcase
+end
+
+reverse_upcase("abc")
+
+def reverse_upcase!(s)
+  s.reverse!
+  s.upcase!
+end
+
+reverse_upcase!("abc")
+
+a = "hello"
+b = "hello"
+a.object_id
+b.object_id
+
+c = b
+
+c.object_id
+
+a.equal?(b)
+b.equal?(c)
