58e48ebf9402833dca5ed9dceee3dd01928eab53
shoichiroo <shoichiroo0601@gmail.com>
2022-06-05 04:41:29 +0000

    Update

--- 4.rb
+++ 4.rb
@@ -122,3 +122,70 @@ end
 
 greet("田中さん", "鈴木さん", "佐藤さん")
 
+a = [1, 2, 3]
+[-1, 0, *a, 4, 5]
+
+%w!apple melon orange!
+
+%w(big\ apple small\ melon orange)
+
+prefix = "This is"
+%W(#{prefix}\ an\ apple small\nmelon orange)
+
+"Ruby".chars
+
+a = Array.new(10){|n| n % 3 + 1}
+
+a = Array.new(5, "default")
+a = Array.new(5){"default"}
+str = a[0]
+str.upcase!
+
+a = "abcde"
+a[2]
+
+fruits = ["apple", "orange", "melon"]
+fruits.each_with_index{|fruit, i| "#{i}: #{fruit}"}
+
+fruits.map.with_index{|fruit, i| "#{i}: #{fruit}"}
+
+fruits.delete_if.with_index{|fruit, i| fruit.include?("a") && i.odd?}
+
+dimensions = [[10, 20], [30, 40], [50, 60]]
+areas = []
+dimensions.each do |length, width|
+  areas << length * width
+end
+
+dimensions.each_with_index do |(length, width), i|
+  "length: #{length}, wkdth: #{width}, i: #{i}"
+end
+
+["1", "20", "300"].map{|s| s.rjust(3, "0")}
+
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n; sum|
+  sum = 10
+  sum += n
+  sum
+end
+
+sum
+
+numbers = [10, 20, 30, 40, 50]
+numbers[2..]
+
+sum = 0
+5.times {|n| sum += n}
+sum
+
+a = []
+10.upto(14) {|n| a << n}
+
+
+a = []
+14.downto(10) {|n| a << n}
+
+a = []
+1.step(10, 2) {|n| a << n}
