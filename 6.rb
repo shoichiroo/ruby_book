b16677a497f94c83d93adb05f8957b4cc329f869
shoichiroo <shoichiroo0601@gmail.com>
2022-06-08 06:02:16 +0000

    Update

--- 6.rb
+++ 6.rb
@@ -41,3 +41,87 @@ text = <<TEXT
 TEXT
 
 text.scan /<option value="([a-z0-9_]+)">(.+)<\/option>/
+
+html = <<-HTML
+  <select name="game_console">
+  <option value="none"></option>
+  <option value="wii_u" selected>Wii U</option>
+  <option value="ps4">プレステ4</option>
+  <option value="gb">ゲームボーイ</option>
+  </select>
+HTML
+
+replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1,\2')
+
+text = <<-TEXT
+def hello(name)
+  puts "Hello, \#{name}!"
+end
+
+hello('Alice')
+
+hello('Bob')
+
+hello('Carol')
+TEXT
+
+text.gsub(/[ \t]+$/, '')
+
+{
+  japan:	'yen',
+  america:'dollar',
+  italy:     'euro'
+}
+
+r = /\d{3}-\d{4}/
+r.class
+
+if "123-4567" =~ /\d{3}-\d{4}/
+  "マッチしました"
+else
+  "マッチしませんでした"
+end
+
+text = "私の誕生日は1977年7月17日です。"
+m = /(\d+)年(\d+)月(\d+)日/.match(text)
+m[3]
+
+if m = /(\d+)年(\d+)月(\d+)日/.match(text)
+  "yes"
+else
+  "no"
+end
+
+m = text.match(/(\d+)年(\d+)月(\d+)日/)
+
+m = text.match(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+m[:year]
+
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+  "#{year}/#{month}/#{day}"
+end
+
+text =~ /(\d+)年(\d+)月(\d+)日/
+$&
+
+"123 456 789".scan(/\d+/)
+
+"1997年7月17日 2021年12月31日".scan(/(?:\d+)年(?:\d+)月(?:\d+)日/)
+
+text = "郵便番号は123-4567 456-7890です。"
+text[/(\d+)-(\d+)/]
+
+text = "誕生日は1997年7月17日です"
+text[/(\d+)年(\d+)月(\d+)日/]
+
+text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/]
+
+text = "123,456-789"
+
+text.split(",")
+
+text.split(/,|-/)
+
+text.gsub(",", ":")
+
+text.gsub(/,|-/, ":")
